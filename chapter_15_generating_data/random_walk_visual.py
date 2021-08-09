17e824ac56e7b780263eeda5cc2feab9a3402fc5
sallyyjan <sallyyujeongan@gmail.com>
2021-08-09 12:45:59 -0700

    created separate scatter and line visualization functions

--- chapter_15_generating_data/random_walk_visual.py
+++ chapter_15_generating_data/random_walk_visual.py
@@ -2,30 +2,71 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Keep making new walks, as long as program is active.
-while True:
-    # Make a random walk
-    rw = RandomWalk(num_points=50_000)
-    rw.fill_walk()
-
-    # Plot the points in the walk
-    plt.style.use('classic')
-    fig, ax = plt.subplots()
-    point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, s=1, c=point_numbers, 
-        cmap=plt.cm.Blues, edgecolors='none')
+class RandomWalkVisualize:
+    """Visualize a random walk using different plot"""
     
-    # Emphasize the first and end points.
-    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
-    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', 
-        s=100)
+    def __init__(self, num_points):
+        """Intialize attributes of a walk."""
+        self.num_points = num_points
+    
+    def visualize_scatter(self):
+        """Visualize random walk using scatter plot"""
 
-    # Remove axis.
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
+        # Keep making new walks, as long as program is active.
+        while True:
+            # Make a random walk
+            rw = RandomWalk(num_points=self.num_points)
+            rw.fill_walk()
 
-    plt.show()
+            # Plot the points in the walk
+            plt.style.use('classic')
+            fig, ax = plt.subplots()
+            point_numbers = range(rw.num_points)
+            ax.scatter(rw.x_values, rw.y_values, s=1, c=point_numbers, 
+                cmap=plt.cm.Blues, edgecolors='none')
+            
+            # Emphasize the first and end points.
+            ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+            ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', 
+                s=100)
 
-    keep_running = input("Make another walk? (y/n): ")
-    if keep_running == 'n':
-        break
+            # Remove axis.
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
+
+            plt.show()
+
+            keep_running = input("Make another walk? (y/n): ")
+            if keep_running == 'n':
+                break
+    
+    def visualize_line(self):
+        """Visualize random walk using a line plot."""
+
+        # Keep making new walks, as long as program is active.
+        while True:
+            # Make a random walk
+            rw = RandomWalk(num_points=self.num_points)
+            rw.fill_walk()
+
+            # Plot the points in the walk
+            plt.style.use('classic')
+            fig, ax = plt.subplots()
+            point_numbers = range(rw.num_points)
+            ax.plot(rw.x_values, rw.y_values, linewidth=1, c='blue')
+
+            # Remove axis.
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
+
+            plt.show()
+
+            keep_running = input("Make another walk? (y/n): ")
+            if keep_running == 'n':
+                break
+
+
+# visualize random walk
+rwv = RandomWalkVisualize(5000)
+rwv.visualize_line()
+rwv.visualize_scatter()
