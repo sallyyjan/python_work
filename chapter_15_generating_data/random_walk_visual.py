2ac8e6a2d0ee3c058cff4529599014ca7b2adc3a
sallyyjan <sallyyujeongan@gmail.com>
2021-08-09 11:19:22 -0700

    path customization

--- chapter_15_generating_data/random_walk_visual.py
+++ chapter_15_generating_data/random_walk_visual.py
@@ -11,7 +11,14 @@ while True:
     # Plot the points in the walk
     plt.style.use('classic')
     fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, s=15, c=point_numbers, 
+        cmap=plt.cm.Blues, edgecolors='none')
+    
+    # Emphasize the first and end points.
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', 
+        s=100)
 
     plt.show()
 
