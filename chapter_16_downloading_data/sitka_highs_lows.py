7a2d817e4c2370405202035a981108bbf2b9a146
sallyyjan <sallyyujeongan@gmail.com>
2021-08-10 20:52:48 -0700

    now compares high and low temperatures

--- chapter_16_downloading_data/sitka_highs_lows.py
+++ chapter_16_downloading_data/sitka_highs_lows.py
@@ -9,19 +9,22 @@ with open(filename) as f:
     header_row = next(reader)
     print(header_row)
 
-    # Get dates and max temperatures
-    highs, dates = [], []
+    # Get dates, and max and min temperatures
+    highs, lows, dates = [], [], []
     for row in reader:
         highs.append(int(row[5]))
+        lows.append(int(row[6]))
         dates.append(datetime.strptime(row[2], '%Y-%m-%d'))
 
-# Plot the max temperatures
+# Plot the max and min temperatures
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c='red', linewidth=1)
+ax.plot(dates, highs, c='red', linewidth=1, alpha=0.5)
+ax.plot(dates, lows, c='blue', linewidth=1, alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot
-plt.title("Daily high temperatures - 2018", fontsize=24)
+plt.title("Daily high and low temperatures - 2018", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel('Temperature (F)', fontsize=16)
