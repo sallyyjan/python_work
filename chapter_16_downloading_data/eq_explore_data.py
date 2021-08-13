d81be1f552ed0cc48771313984546a25de88665b
sallyyjan <sallyyujeongan@gmail.com>
2021-08-12 21:06:25 -0700

    color customization and added hovertext

--- chapter_16_downloading_data/eq_explore_data.py
+++ chapter_16_downloading_data/eq_explore_data.py
@@ -4,7 +4,7 @@ from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
 # Explore the structure of the json formatted data
-filename = 'data/eq_data_1_day_m1.json'
+filename = 'data/eq_data_30_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f) # a giant dictionary
 
@@ -15,14 +15,27 @@ with open(readable_file, 'w') as f:
 
 all_eq_dicts = all_eq_data['features'] 
 
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_text = [], [], [], []
 for eq_dict in all_eq_dicts:
     mags.append(eq_dict['properties']['mag'])
     lons.append(eq_dict['geometry']['coordinates'][0])
     lats.append(eq_dict['geometry']['coordinates'][1])
+    hover_text.append(eq_dict['properties']['title'])
 
 # Map the earthquakes.
-data = [Scattergeo(lon=lons, lat=lats)]
+data = [{
+    'type': 'scattergeo',
+    'lon': lons, 
+    'lat': lats,
+    'text': hover_text,
+    'marker': {
+        'size': [5*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Viridis',
+        'reversescale': True,
+        'colorbar': {'title': "Magnitude"},
+    },
+}]
 my_layout = Layout(title='Global Earthquakes')
 
 fig = {'data': data, 'layout': my_layout}
