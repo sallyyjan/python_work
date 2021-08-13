f4d754d24d3d5cbd67c0ff81ef6f4df5c1552911
sallyyjan <sallyyujeongan@gmail.com>
2021-08-13 12:07:59 -0700

    get article from API

---
+++ chapter_17_working_with_APIs/hackernews_submissions.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+# get request from hackernews API
+url = "https://hacker-news.firebaseio.com/v0/item/19155826.json"
+r = requests.get(url)
+print(f"Status code: {r.status_code}")
+
+# create python dictionary of response
+respose_dict = r.json()
+readable_file = 'readable_hackernews_data.json'
+with open(readable_file, 'w') as f:
+    json.dump(respose_dict, f, indent=4)
