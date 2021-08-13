b9d07db77baf124113a88b1c6ba9db08c5f151a7
sallyyjan <sallyyujeongan@gmail.com>
2021-08-13 12:21:23 -0700

    APU resqest for each aritcle

--- chapter_17_working_with_APIs/hackernews_submissions.py
+++ chapter_17_working_with_APIs/hackernews_submissions.py
@@ -1,13 +1,29 @@
+from operator import itemgetter
+
 import requests
 import json
 
-# get request from hackernews API
+from requests.models import Response
+
+# Make API call and store response.
 url = "https://hacker-news.firebaseio.com/v0/topstories.json"
 r = requests.get(url)
 print(f"Status code: {r.status_code}")
 
-# create python dictionary of response
-respose_dict = r.json()
-readable_file = 'readable_hackernews_data.json'
-with open(readable_file, 'w') as f:
-    json.dump(respose_dict, f, indent=4)
+# Process information about each submission.
+submission_ids = r.json()
+submission_dicts = []
+for submission_id in submission_ids[:30]:
+    # Make separate API call for each submission.
+    url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
+    r = requests.get(url)
+    print(f"submission: {submission_id}\t status: {r.status_code}")
+    response_dict = r.json()
+
+    # Build a dictionary for each article.
+    submission_dict = {
+        'title': response_dict['title'],
+        'hn_link': f"http://news.ycombinator.com/item?id={submission_id}",
+        'comments': response_dict['decendants'],
+    }
+    submission_dicts.append(submission_dict)
