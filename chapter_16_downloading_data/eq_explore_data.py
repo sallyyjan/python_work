b74d3858ae7c74a14ad39fb78230638fb89cfc5d
sallyyjan <sallyyujeongan@gmail.com>
2021-08-12 20:38:03 -0700

    extracting coordinates

--- chapter_16_downloading_data/eq_explore_data.py
+++ chapter_16_downloading_data/eq_explore_data.py
@@ -12,8 +12,10 @@ with open(readable_file, 'w') as f:
 
 all_eq_dicts = all_eq_data['features'] 
 
-mags = []
+mags, lons, lats = [], [], []
 for eq_dict in all_eq_dicts:
     mags.append(eq_dict['properties']['mag'])
+    lons.append(eq_dict['geometry']['coordinates'][0])
+    lats.append(eq_dict['geometry']['coordinates'][1])
 
-print(mags)
+print(lons[:10], lats[:10])
