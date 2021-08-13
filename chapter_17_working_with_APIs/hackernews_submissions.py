a06e32c04f015dff7002cfc79fb03b1989f42ed1
sallyyjan <sallyyujeongan@gmail.com>
2021-08-13 12:36:28 -0700

    ignore incomplete submissions

--- chapter_17_working_with_APIs/hackernews_submissions.py
+++ chapter_17_working_with_APIs/hackernews_submissions.py
@@ -1,7 +1,6 @@
 from operator import itemgetter
 
 import requests
-import json
 
 from requests.models import Response
 
@@ -21,9 +20,25 @@ for submission_id in submission_ids[:30]:
     response_dict = r.json()
 
     # Build a dictionary for each article.
-    submission_dict = {
+    # Ignore submssions that have no descendant key
+    try:
+        comments = response_dict['descendants']
+    except KeyError:
+        print(f"submission: {submission_id} missing 'descendants' key")
+        print(f"submission: {submission_id} will be ignored")
+    else:
+        submission_dict = {
         'title': response_dict['title'],
         'hn_link': f"http://news.ycombinator.com/item?id={submission_id}",
-        'comments': response_dict['decendants'],
+        'comments': comments,
     }
+
     submission_dicts.append(submission_dict)
+
+submission_dicts = sorted(submission_dicts, key=itemgetter('comments'), 
+                            reverse=True)
+
+for submission_dict in submission_dicts:
+    print(f"\nTitle: {submission_dict['title']}")
+    print(f"Discussion link: {submission_dict['hn_link']}")
+    print(f"Comments: {submission_dict['comments']}")
